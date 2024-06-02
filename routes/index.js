43aa502ae262e4c0a640fe2c875dd82facbedfe1
Anderson Machado <anderson361163@gmail.com>
2024-06-02 17:52:52 -0300

    feat: Adicionando o mysql e reformulando o processo de listagem de links

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('../db');
 
-/* GET home page. */
-/*
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});*/
+const links = {
+  links: [
+    { href: '/', text: 'Inicio' },
+    { href: '/lista', text: 'Link 2' }
+  ]
+};
+
+const data = {
+  people: [
+      { name: "John Doe", age: 25, city: "New York" },
+      { name: "Jane Smith", age: 30, city: "San Francisco" },
+      { name: "Michael Johnson", age: 35, city: "Los Angeles" }
+  ]
+};
 
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('partials/lista/lista', 
+    { 
+      ...links,
+      ...data,
+      title: 'Fiap Blog',
+    }   
+    );
 });
+
+router.get('/lista', function(req, res, next) {
+  res.render('login', 
+    { 
+      ...links,
+      title: 'Fiap Blog',
+    }
+    );
+});
+
 
 module.exports = router;
